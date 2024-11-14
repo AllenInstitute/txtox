e90a33c9095b9b5d174dce04663fea2030b526c6
rhngla <rhngla@gmail.com>
2024-11-14 12:43:35 -0800

    added accidentally untracked files

---
+++ txtox/trainers/gnn_het_reg_2d.py
@@ -0,0 +1,50 @@
+import lightning as L
+from lightning.pytorch.callbacks import ModelCheckpoint
+from lightning.pytorch.loggers import TensorBoardLogger
+
+from txtox.data.datamodules import AnnDataGraphDataModule
+from txtox.models.gnn_het_reg_2d import LitGNNHetReg2d
+from txtox.utils import get_datetime, get_paths
+
+
+def main():
+    # data parameters, we'll eventually obtain this from the data.
+    n_genes = 500
+    n_labels = 126
+
+    # paths
+    paths = get_paths()
+    expname = get_datetime(expname="VISp_nhood_GNNhetReg_attres_2d")
+    log_path = paths["data_root"] + f"logs/{expname}"
+    checkpoint_path = paths["data_root"] + f"checkpoints/{expname}"
+
+    # helpers
+    tb_logger = TensorBoardLogger(save_dir=log_path)
+    checkpoint_callback = ModelCheckpoint(
+        dirpath=checkpoint_path, monitor="val_rmse_overall", filename="{epoch}-{val_rmse_overall:.2f}"
+    )
+
+    # data
+    datamodule = AnnDataGraphDataModule(
+        data_dir=paths["data_root"],
+        file_names=["VISp_nhood.h5ad"],
+        spatial_coords=["x_section", "y_section", "z_section"],
+        batch_size=1,
+    )
+
+    # model
+    model = LitGNNHetReg2d(input_dim=n_genes, n_labels=n_labels, weight_gnll=1.0, weight_ce=0.1)
+
+    # fit wrapper
+    trainer = L.Trainer(
+        limit_train_batches=1000,
+        limit_val_batches=100,
+        max_epochs=200,
+        logger=tb_logger,
+        callbacks=[checkpoint_callback],
+    )
+    trainer.fit(model=model, datamodule=datamodule)
+
+
+if __name__ == "__main__":
+    main()
